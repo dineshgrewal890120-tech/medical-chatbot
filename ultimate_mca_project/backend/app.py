79e920f5c528ce1be000e36f2f35d2f39da3d9ea
dineshgrewal890120-tech <dineshgrewal890120@gmail.com>
2026-03-18 09:36:20 +0530

    Add files via upload

---
+++ ultimate_mca_project/backend/app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    return jsonify([
+        {"disease":"Flu","probability":80,"advice":"Rest and hydration"},
+        {"disease":"Cold","probability":60,"advice":"Take warm fluids"},
+        {"disease":"Migraine","probability":40,"advice":"Avoid stress"}
+    ])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
